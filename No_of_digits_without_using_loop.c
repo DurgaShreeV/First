4717eea8085f5a4b3b9d2a032df95555b56043fe
DurgaShreeV <140792824+DurgaShreeV@users.noreply.github.com>
2023-08-02 13:43:35 +0530

    No_of_digits_without_using_loop.c

---
+++ No_of_digits_without_using_loop.c
@@ -0,0 +1,11 @@
+/*No of digits without using loop using C*/
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int n,no_of_digits;
+    scanf("%d%n",&n,&no_of_digits);       //--->Integer     
+    //scanf("%s%n",s,&no_of_digits);        --->String
+    printf("%d",no_of_digits);
+    return 0;
+}
