54470870a222402d9ca5ad885cb074d875413cb2
DurgaShreeV <140792824+DurgaShreeV@users.noreply.github.com>
2023-08-02 13:14:56 +0530

    Sub_Palindromes_Count.c

---
+++ Sub_Palindromes_Count.c
@@ -0,0 +1,39 @@
+/*Sub Palindromes Count using C*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    char s[200];
+    int c=0;
+    scanf("%s",s);
+    for(int i=0;i<strlen(s);i++)
+    {
+        for(int j=strlen(s)-1;j>i;j--)
+        {
+            if(s[i]==s[j])
+            {
+                int flag=0;
+                for(int l=i,r=j;l<r;l++,r--)
+                {
+                    if(s[l]==s[r])
+                    {
+                        printf("%c",s[l]);
+                    }
+                    else
+                    {
+                        printf("\n");
+                        flag=1;
+                        break;
+                    }
+                }
+                if(flag==0)
+                {
+                    c++;
+                }
+            }
+        }
+    }
+    printf("%d",c);
+}
+
